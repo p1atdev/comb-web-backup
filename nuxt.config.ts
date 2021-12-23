ed50c11c5628cc0693a4ec83761d60b3f43e6bec
Shu Teiei <shu_teiei@outlook.jp>
2021-12-23 14:51:31 +0900

    removed baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { defineNuxtConfig } from "nuxt3"
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    env: {
-        baseUrl: process.env.BASE_URL || "http://localhost:3000",
-    },
     meta: {
         title: "海城コンピューター部",
         htmlAttrs: {

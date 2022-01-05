1f2e447e12d176ca74429615c7154776a287829b
p1atdev <shu_teiei@outlook.jp>
2022-01-05 15:36:24 +0900

    fix: show navbar

--- src/components/NavBar/NavBarItems.vue
+++ src/components/NavBar/NavBarItems.vue
@@ -14,11 +14,11 @@ const { searchButtonClicked, hideItemsWhenSM } = defineProps<Props>()
         :class="hideItemsWhenSM ? 'hidden' : 'flex'"
         class="sm:flex justify-around items-center space-x-6 lg:mx-8 xl:mx-12"
     >
-        <NavBarItemRow title="概要" to="/about" />
-        <NavBarItemRow title="作品" to="/about" />
-        <NavBarItemRow title="記事" to="/about" />
-        <NavBarItemRow title="実績" to="/about" />
-        <NavBarItemRow title="文化祭" to="/about" />
+        <NavBarItemsRow title="概要" to="/about" />
+        <NavBarItemsRow title="作品" to="/about" />
+        <NavBarItemsRow title="記事" to="/about" />
+        <NavBarItemsRow title="実績" to="/about" />
+        <NavBarItemsRow title="文化祭" to="/about" />
         <!-- TODO: これを押したらAlgoliaの検索のポップアップを出す -->
         <button
             class="w-8 h-8 lg:w-24 lg:px-2 bg-stone-100 text-gray-600 hover:shadow-sm rounded-full border-2 border-transparent hover:border-primary flex justify-between items-center"
